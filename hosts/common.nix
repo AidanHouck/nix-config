8dcec98837e8bf16d069833fd0f10a4b249f847f
Aidan Houck <git@aidanhouck.com>
2024-12-14 16:11:06 +0000

    feat: add gh

--- hosts/common.nix
+++ hosts/common.nix
@@ -10,6 +10,7 @@
   sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/home/houck/.config/sops/age/keys.txt";
   sops.secrets."wireless.env" = { };
+  sops.secrets."github_token" = { };
 
   # Enable Flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -17,6 +18,7 @@
   # Common software
   environment.systemPackages = with pkgs; [
     git # Must init git first for Flakes dependency cloning
+    gh # GitHub CLI
     vim
     wget
   ];
