337dd64f0f4ab8d84104aab019ecf55e63d4b2ee
Aidan Houck <AidanHouck@users.noreply.github.com>
2024-12-28 03:50:39 +0000

    feat: add auto-gc

--- hosts/common.nix
+++ hosts/common.nix
@@ -25,4 +25,12 @@
     tree
   ];
   environment.variables.EDITOR = "vim";
+
+  boot.loader.systemd-boot.configurationLimit = 10;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+  nix.settings.auto-optimise-store = true;
 }
