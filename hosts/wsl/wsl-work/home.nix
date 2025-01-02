b19b53b6c4a19c39d2b772fefbe95efd0469d0cf
Aidan Houck <AidanHouck@users.noreply.github.com>
2025-01-02 20:54:52 +0000

    feat: enable zsh on work wsl machine

--- hosts/wsl/wsl-work/home.nix
+++ hosts/wsl/wsl-work/home.nix
@@ -9,6 +9,10 @@
     ./../../../home/common.nix
   ];
 
+  # Enable ZSH
+  aidan.home.shell.zsh.enable = true;
+  aidan.home.shell.bash.enable = false;
+
   # Packages for this host only
   home.packages = with pkgs; [
     # TODO
