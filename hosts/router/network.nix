79d874b77ad8ce54b770ef789fbeef673c35afee
Aidan Houck <AidanHouck@users.noreply.github.com>
2025-01-09 03:22:39 +0000

    feat: add (mostly) real vlans

--- hosts/router/network.nix
+++ hosts/router/network.nix
@@ -28,7 +28,7 @@ in {
 
     networking = {
       useDHCP = false;
-      nameservers = [ "192.168.30.250" "192.168.30.251" "fd00:30::250" "fd00:30::251" ];
+      nameservers = ["192.168.30.250" "192.168.30.251" "fd00:30::250" "fd00:30::251"];
       vlans = {
         "${lan}.10" = {
           id = 10;
@@ -39,27 +39,86 @@ in {
           id = 20;
           interface = "${lan}";
         };
+
+        "${lan}.21" = {
+          id = 21;
+          interface = "${lan}";
+        };
+
+        "${lan}.25" = {
+          id = 25;
+          interface = "${lan}";
+        };
+
+        "${lan}.30" = {
+          id = 30;
+          interface = "${lan}";
+        };
+
+        "${lan}.40" = {
+          id = 40;
+          interface = "${lan}";
+        };
       };
 
       interfaces = {
         ${wan}.useDHCP = true;
 
         "${lan}.10" = {
-          ipv4.addresses = [{
-            address = "10.10.10.1";
-            prefixLength = 24;
-          }];
+          ipv4.addresses = [
+            {
+              address = "192.168.100.3";
+              prefixLength = 24;
+            }
+          ];
         };
 
         "${lan}.20" = {
-          ipv4.addresses = [{
-            address = "10.10.20.1";
-            prefixLength = 24;
-          }];
+          ipv4.addresses = [
+            {
+              address = "192.168.20.3";
+              prefixLength = 24;
+            }
+          ];
+        };
+
+        "${lan}.21" = {
+          ipv4.addresses = [
+            {
+              address = "192.168.21.3";
+              prefixLength = 24;
+            }
+          ];
+        };
+
+        "${lan}.25" = {
+          ipv4.addresses = [
+            {
+              address = "192.168.25.3";
+              prefixLength = 24;
+            }
+          ];
+        };
+
+        "${lan}.30" = {
+          ipv4.addresses = [
+            {
+              address = "192.168.30.3";
+              prefixLength = 24;
+            }
+          ];
+        };
+
+        "${lan}.40" = {
+          ipv4.addresses = [
+            {
+              address = "192.168.40.3";
+              prefixLength = 24;
+            }
+          ];
         };
       };
     };
-
 
     system.stateVersion = "24.11";
   };
