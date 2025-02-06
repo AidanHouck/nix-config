68a280f3afa4afdb88a1646813792fdb29d2a700
Aidan Houck <AidanHouck@users.noreply.github.com>
2025-02-06 16:08:35 +0000

    feat: add `kube-*` wsl-work ssh config
    mostly for autocompletion. `ssh ku<TAB>ser<TAB>1<ENTER>`

--- hosts/wsl/wsl-work/default.nix
+++ hosts/wsl/wsl-work/default.nix
@@ -21,6 +21,29 @@ in {
       # TODO
     ];
 
+    programs.ssh.extraConfig = ''
+      Host kube-server-01
+          HostName kube-server-01.mveca.org
+
+      Host kube-server-02
+          HostName kube-server-02.mveca.org
+
+      Host kube-server-03
+          HostName kube-server-03.mveca.org
+
+      Host kube-agent-01
+          HostName kube-agent-01.mveca.org
+
+      Host kube-agent-02
+          HostName kube-agent-02.mveca.org
+
+      Host kube-agent-03
+          HostName kube-agent-03.mveca.org
+
+      Host kube-*
+          User houck.admin
+    '';
+
     system.stateVersion = "24.05";
   };
 }
