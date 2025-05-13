9b93ebfa70247b6128b41eb1034d0ba14391713d
Aidan Houck <AidanHouck@users.noreply.github.com>
2025-05-13 10:46:01 -0400

    feat: add ssh logging to tmux C-o panel
    Files should be named `base/ssh.d/District/Hostname`
    with content `ssh -FLAGS user@IP` / `telnet IP`

--- modules/home/cli/tmux/tmux-ssh.sh
+++ modules/home/cli/tmux/tmux-ssh.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
-ssh_dir='/mnt/o/Aidan/ssh.d/'
+share_base='/mnt/o/Aidan'
+ssh_dir="${share_base}/ssh.d/"
+log_dir="${share_base}/puttylogs/"
 
-pushd $ssh_dir 1>/dev/null || exit
+pushd "$ssh_dir" 1>/dev/null || exit
 result=$(rg '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)' -or '$1' | fzf --print0 | cut -d':' -f1)
 popd 1>/dev/null || exit
 
 if [[ $result ]]; then
-	tmux new-window -n "$(basename $result)" bash ${ssh_dir}${result}
+	hostname=$(basename "$result")
+	district=$(dirname "$result")
+	ip=$(grep -Eo '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)' "${ssh_dir}${result}")
+
+	logname="${log_dir}${district}/${hostname}_${ip}-$(date +%Y-%m-%d-%H%M%S).log"
+	mkdir -p "${log_dir}${district}"
+
+	tmux new-window -n "$hostname" "bash ${ssh_dir}${result} | tee $logname"
 fi
 
