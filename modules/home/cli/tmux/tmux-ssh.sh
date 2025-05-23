b1e8e2aef2a93e8d4d4c7276967fb6132a85ca62
Aidan Houck <AidanHouck@users.noreply.github.com>
2025-05-23 10:40:18 -0400

    fix: handle connections to fqdns properly

--- modules/home/cli/tmux/tmux-ssh.sh
+++ modules/home/cli/tmux/tmux-ssh.sh
@@ -3,16 +3,24 @@ share_base='/mnt/o/Aidan'
 ssh_dir="${share_base}/ssh.d/"
 log_dir="${share_base}/puttylogs/"
 
+connection_ip_regex='([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)'
+connection_host_regex='((?:[a-zA-Z0-9\-]+[.])*[a-zA-Z0-9]+[.](?:com|org))'
+
 pushd "$ssh_dir" 1>/dev/null || exit
-result=$(rg '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)' -or '$1' | fzf --print0 | cut -d':' -f1)
+result=$(rg -P -e "$connection_ip_regex" -e "$connection_host_regex" -or '$1$2' | fzf --print0 | cut -d':' -f1)
 popd 1>/dev/null || exit
 
 if [[ $result ]]; then
 	hostname=$(basename "$result")
 	district=$(dirname "$result")
-	ip=$(grep -Eo '([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)' "${ssh_dir}${result}")
+	ip=$(grep -Eo -e "$connection_ip_regex" "${ssh_dir}${result}")
 
-	logname="${log_dir}${district}/${hostname}_${ip}-$(date +%Y-%m-%d-%H%M%S).log"
+	if [[ $ip ]]; then
+		logname="${log_dir}${district}/${hostname}_${ip}-$(date +%Y-%m-%d-%H%M%S).log"
+	else
+		logname="${log_dir}${district}/${hostname}-$(date +%Y-%m-%d-%H%M%S).log"
+	fi
+
 	mkdir -p "${log_dir}${district}"
 
 	tmux new-window -n "$hostname" "\
