38bf9279a7a8c4a6618965435ddf8ac0a4da23a9
Aidan Houck <AidanHouck@users.noreply.github.com>
2025-08-11 12:34:50 -0400

    feat: add tmux ssh multiplexing
    use tab/shift+tab to select multiple fzf results

--- modules/home/cli/tmux/tmux-ssh-window.sh
+++ modules/home/cli/tmux/tmux-ssh-window.sh
@@ -7,13 +7,23 @@ connection_ip_regex='([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)'
 connection_host_regex='((?:[a-zA-Z0-9\-]+[.])*[a-zA-Z0-9]+[.](?:com|org))'
 
 pushd "$ssh_dir" 1>/dev/null || exit
-result=$(rg -P -e "$connection_ip_regex" -e "$connection_host_regex" -or '$1$2' | fzf --print0 | cut -d':' -f1)
+result=$(rg -P -e "$connection_ip_regex" -e "$connection_host_regex" -or '$1$2' | fzf -m | cut -d':' -f1)
 popd 1>/dev/null || exit
 
-if [[ $result ]]; then
-	hostname=$(basename "$result")
-	district=$(dirname "$result")
-	ip=$(grep -Eo -e "$connection_ip_regex" "${ssh_dir}${result}")
+if [[ ! $result ]]; then
+	exit
+fi
+
+multiple_results=
+if [[ $(echo "$result" | wc -l) -gt 1 ]]; then
+	multiple_results=1
+fi
+
+i=0
+while IFS= read -r line; do
+	hostname=$(basename "$line")
+	district=$(dirname "$line")
+	ip=$(grep -Eo -e "$connection_ip_regex" "${ssh_dir}${line}")
 
 	if [[ $ip ]]; then
 		logname="${log_dir}${district}/${hostname}_${ip}-$(date +%Y-%m-%d-%H%M%S).log"
@@ -23,7 +33,24 @@ if [[ $result ]]; then
 
 	mkdir -p "${log_dir}${district}"
 
-	tmux new-window -n "$hostname" \
-		"~/.config/tmux/tmux-ssh-loop.sh '$result' '$logname' '$ssh_dir' '$user'"
+	if [[ $i -eq 0 ]]; then
+		# First run, create window
+		tmux new-window -n "$hostname" \
+			"~/.config/tmux/tmux-ssh-loop.sh '$line' '$logname' '$ssh_dir' '$user'"
+	else
+		# Subsequent run, split window
+		tmux split-window \
+			"~/.config/tmux/tmux-ssh-loop.sh '$line' '$logname' '$ssh_dir' '$user'"
+	fi
+
+	i=$((i+1))
+done <<< "$result"
+
+if [[ $multiple_results ]]; then
+	# Set evenly-spaced tile layout
+	tmux select-layout tiled
+
+	# Sync keyboard input to all panes
+	tmux set-window-option synchronize-panes on
 fi
 
