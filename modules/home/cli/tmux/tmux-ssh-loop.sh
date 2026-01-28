71d979c02791ec2739e57d471cebce513b6fc233
Aidan Houck <AidanHouck@users.noreply.github.com>
2026-01-28 14:27:23 -0500

    feat: start vim at line of offending key
    if +num has no num then it will default to bottom of the file

--- modules/home/cli/tmux/tmux-ssh-loop.sh
+++ modules/home/cli/tmux/tmux-ssh-loop.sh
@@ -3,18 +3,19 @@ result="$1"
 logname="$2"
 ssh_dir="$3"
 user="$4"
+temp="$(mktemp)"
 
 menu () {
 	echo "Connect has terminated. Select an option to continue."
 
 	finish="-1"
 	while [ "$finish" = "-1" ]; do
-		echo -e "1: Retry\n2: Copy output to clipboard\n3: Edit ~/.ssh/known_hosts\n4: Exit"
+		echo -e "1: Retry\n2: Copy output to clipboard\n3: vim ~/.ssh/known_hosts\n4: Exit"
 		read -rp "Selection: " choice
 		case "$choice" in
-		  1|r ) ssh;;
+		  1|r ) rm -f "$temp"; ssh;;
 		  2|c ) copy;;
-		  3|s|h ) hosts;;
+		  3|v ) hosts;;
 		  ""|4|q ) finish=1; exit;;
 		esac
 	done
@@ -23,7 +24,7 @@ menu () {
 ssh () {
 	echo "${result}" | tee -a "$logname"
 	tee -a "$logname" < "${ssh_dir}${result}"
-	bash -c "$(sed "s/DSL_LAST/${user}/g" "${ssh_dir}${result}")" | tee -a "$logname"
+	bash -c "$(sed "s/DSL_LAST/${user}/g" "${ssh_dir}${result}")" 2>&1 | tee -a "$logname" "$temp"
 }
 
 copy () {
@@ -32,7 +33,8 @@ copy () {
 }
 
 hosts () {
-	vim ~/.ssh/known_hosts
+	line=$(cat "$temp" | grep -Eo 'known_hosts:[0-9]+' | sed 's/known_hosts://')
+	vim "+$line" ~/.ssh/known_hosts
 }
 
 ssh
