68a5983365f5210fedd6f946312c2758a53821d1
Aidan Houck <AidanHouck@users.noreply.github.com>
2026-04-13 11:42:29 -0400

    feat: use underline escape sequences in ssh script

--- modules/home/cli/tmux/tmux-ssh-loop.sh
+++ modules/home/cli/tmux/tmux-ssh-loop.sh
@@ -11,11 +11,11 @@ menu () {
 
 	finish="-1"
 	while [ "$finish" = "-1" ]; do
-		echo -e "1: [r]etry
-2: [c]opy output to clipboard
-3: [v]im ~/.ssh/known_hosts
-4: [e]dit $result
-5: [Q]uit"
+		echo -e "1: \e[4mR\e[0metry
+2: \e[4mC\e[0mopy output to clipboard
+3: \e[4mv\e[0mim ~/.ssh/known_hosts
+4: \e[4mE\e[0mdit $result
+5: \e[4mQ\e[0muit"
 		read -rp "Selection: " choice
 		case "$choice" in
 		  1|r|R ) ssh;;
